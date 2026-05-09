98d573045327716a574bbba8946ae4ea2a87f9c5
ikageng tladi <ikagengt.dev@gmail.com>
2026-05-09 10:34:23 +0200

    feat: add IContractRepository and ContractRepository

---
+++ GLMS.Web/Repositories/Contracts/IContractRepository.cs
@@ -0,0 +1,14 @@
+﻿using GLMS.Web.Models;
+
+namespace GLMS.Web.Repositories.Contracts
+{
+    public interface IContractRepository
+    {
+        IEnumerable<Contract> GetAll();
+        Contract? GetById(int id);
+        void Add(Contract contract);
+        void Update(Contract contract);
+        void Delete(int id);
+        void Save();
+    }
+}
