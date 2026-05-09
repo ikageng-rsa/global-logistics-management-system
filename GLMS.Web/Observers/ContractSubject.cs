917e0ec1082831bc0c394177b8913486e65f4977
ikageng tladi <ikagengt.dev@gmail.com>
2026-05-09 12:09:13 +0200

    feat: add AuditLogger observer

---
+++ GLMS.Web/Observers/ContractSubject.cs
@@ -0,0 +1,28 @@
+﻿using GLMS.Web.Models;
+using GLMS.Web.Observers.Contracts;
+
+namespace GLMS.Web.Observers
+{
+    public class ContractSubject
+    {
+        private readonly List<IContractObserver> _observers = new();
+
+        public void Attach(IContractObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(IContractObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify(Contract contract)
+        {
+            foreach (var observer in _observers)
+            {
+                observer.OnStatusChanged(contract);
+            }
+        }
+    }
+}
