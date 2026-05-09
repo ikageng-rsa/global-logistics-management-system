3df4b96d60dd5f8ce769fb44bf1ec7c0d2cc30e0
ikageng tladi <ikagengt.dev@gmail.com>
2026-05-09 10:37:38 +0200

    feat: add IServiceRequestRepository and ServiceRequestRepository

---
+++ GLMS.Web/Repositories/Contracts/IServiceRequestRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using GLMS.Web.Models;
+
+namespace GLMS.Web.Repositories.Contracts
+{
+    public interface IServiceRequestRepository
+    {
+        IEnumerable<ServiceRequest> GetAll();
+        IEnumerable<ServiceRequest> GetByContractId(int contractId);
+        ServiceRequest? GetById(int id);
+        void Add(ServiceRequest serviceRequest);
+        void Update(ServiceRequest serviceRequest);
+        void Delete(int id);
+        void Save();
+    }
+}
---
+++ GLMS.Web/Repositories/ServiceRequestRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GLMS.Web.Data;
+using GLMS.Web.Models;
+using GLMS.Web.Repositories.Contracts;
+
+namespace GLMS.Web.Repositories
+{
+    public class ServiceRequestRepository : IServiceRequestRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ServiceRequestRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<ServiceRequest> GetAll()
+        {
+            return _context.ServiceRequests
+                .Include(sr => sr.Contract)
+                .ToList();
+        }
+
+        public IEnumerable<ServiceRequest> GetByContractId(int contractId)
+        {
+            return _context.ServiceRequests
+                .Where(sr => sr.ContractId == contractId)
+                .ToList();
+        }
+
+        public ServiceRequest? GetById(int id)
+        {
+            return _context.ServiceRequests
+                .Include(sr => sr.Contract)
+                .FirstOrDefault(sr => sr.Id == id);
+        }
+
+        public void Add(ServiceRequest serviceRequest)
+        {
+            _context.ServiceRequests.Add(serviceRequest);
+        }
+
+        public void Update(ServiceRequest serviceRequest)
+        {
+            _context.ServiceRequests.Update(serviceRequest);
+        }
+
+        public void Delete(int id)
+        {
+            var sr = _context.ServiceRequests.Find(id);
+            if (sr != null)
+                _context.ServiceRequests.Remove(sr);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
