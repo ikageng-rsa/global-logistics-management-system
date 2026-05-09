ef2156b87b19ea9afaad01e6f5d1c93182e60193
ikageng tladi <ikagengt.dev@gmail.com>
2026-05-09 10:30:24 +0200

    feat: add IClientRepository and ClientRepository

---
+++ GLMS.Web/Repositories/ClientRepository.cs
@@ -0,0 +1,48 @@
+﻿using GLMS.Web.Data;
+using GLMS.Web.Models;
+using GLMS.Web.Repositories.Contracts;
+
+namespace GLMS.Web.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ClientRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Client> GetAll()
+        {
+            return _context.Clients.ToList();
+        }
+
+        public Client? GetById(int id)
+        {
+            return _context.Clients.Find(id);
+        }
+
+        public void Add(Client client)
+        {
+            _context.Clients.Add(client);
+        }
+
+        public void Update(Client client)
+        {
+            _context.Clients.Update(client);
+        }
+
+        public void Delete(int id)
+        {
+            var client = _context.Clients.Find(id);
+            if (client != null)
+                _context.Clients.Remove(client);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
