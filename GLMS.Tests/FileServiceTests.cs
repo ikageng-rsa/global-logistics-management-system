9c540aacee03e1a6a419dcb85cb9717e7f1599d9
ikageng tladi <ikagengt.dev@gmail.com>
2026-05-13 19:39:16 +0200

    test: add FileServiceTests class setup

---
+++ GLMS.Tests/FileServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using GLMS.Web.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace GLMS.Tests
+{
+    public class FileServiceTests
+    {
+        private readonly Mock<IWebHostEnvironment> _environmentMock;
+        private readonly Mock<ILogger<FileService>> _loggerMock;
+        private readonly FileService _fileService;
+
+        public FileServiceTests()
+        {
+            _environmentMock = new Mock<IWebHostEnvironment>();
+            _loggerMock = new Mock<ILogger<FileService>>();
+
+            // Point WebRootPath to a temp folder for testing
+            _environmentMock.Setup(e => e.WebRootPath)
+                .Returns(Path.GetTempPath());
+
+            _fileService = new FileService(_environmentMock.Object, _loggerMock.Object);
+        }
+    }
+}
