bbeaf8ce343d46695f0ca56fbd031150218d248b
ikageng tladi <ikagengt.dev@gmail.com>
2026-05-13 19:35:31 +0200

    test: add http failing and mock helpers

---
+++ GLMS.Tests/Helpers/FailingHttpMessageHandler.cs
@@ -0,0 +1,13 @@
+﻿
+namespace GLMS.Tests.Helpers
+{
+    public class FailingHttpMessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            throw new HttpRequestException("Simulated API failure.");
+        }
+    }
+}
---
+++ GLMS.Tests/Helpers/MockHttpMessageHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Text;
+
+namespace GLMS.Tests.Helpers
+{
+    public class MockHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly decimal _rate;
+
+        public MockHttpMessageHandler(decimal rate)
+        {
+            _rate = rate;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            var json = $@"{{
+                ""result"": ""success"",
+                ""rates"": {{
+                    ""ZAR"": {_rate}
+                }}
+            }}";
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
