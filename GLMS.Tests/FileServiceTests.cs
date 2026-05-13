ce5a8f2f43b6a37350be951866e954652574493b
ikageng tladi <ikagengt.dev@gmail.com>
2026-05-13 19:45:24 +0200

    test: add fileservice tests including including valid pdf file, non pdf file, large pdf file and null file

--- GLMS.Tests/FileServiceTests.cs
+++ GLMS.Tests/FileServiceTests.cs
@@ -23,5 +23,52 @@ namespace GLMS.Tests
 
             _fileService = new FileService(_environmentMock.Object, _loggerMock.Object);
         }
+
+        [Fact]
+        public void IsValidPdf_ValidPdfFile_ReturnsTrue()
+        {
+            var fileMock = new Mock<IFormFile>();
+            fileMock.Setup(f => f.FileName).Returns("agreement.pdf");
+            fileMock.Setup(f => f.ContentType).Returns("application/pdf");
+            fileMock.Setup(f => f.Length).Returns(1024);
+
+            var result = _fileService.IsValidPdf(fileMock.Object);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsValidPdf_NonPdfFile_ReturnsFalse()
+        {
+            var fileMock = new Mock<IFormFile>();
+            fileMock.Setup(f => f.FileName).Returns("malicious.exe");
+            fileMock.Setup(f => f.ContentType).Returns("application/octet-stream");
+            fileMock.Setup(f => f.Length).Returns(1024);
+
+            var result = _fileService.IsValidPdf(fileMock.Object);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValidPdf_FileTooLarge_ReturnsFalse()
+        {
+            var fileMock = new Mock<IFormFile>();
+            fileMock.Setup(f => f.FileName).Returns("large.pdf");
+            fileMock.Setup(f => f.ContentType).Returns("application/pdf");
+            fileMock.Setup(f => f.Length).Returns(11 * 1024 * 1024);
+
+            var result = _fileService.IsValidPdf(fileMock.Object);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValidPdf_NullFile_ReturnsFalse()
+        {
+            var result = _fileService.IsValidPdf(null!);
+
+            Assert.False(result);
+        }
     }
 }
