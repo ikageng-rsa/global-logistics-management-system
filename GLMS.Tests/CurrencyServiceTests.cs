8fced4157ab50a4471649b2ff688cd00d836383f
ikageng tladi <ikagengt.dev@gmail.com>
2026-05-13 19:21:07 +0200

    test: add CurrencyServiceTests class with setup

---
+++ GLMS.Tests/CurrencyServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using GLMS.Web.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLMS.Tests
+{
+    public class CurrencyServiceTests
+    {
+        private readonly Mock<ILogger<ExchangeRateService>> _loggerMock;
+
+        public CurrencyServiceTests()
+        {
+            _loggerMock = new Mock<ILogger<ExchangeRateService>>();
+        }
+    }
+}
