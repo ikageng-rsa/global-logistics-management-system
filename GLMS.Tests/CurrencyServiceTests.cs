0d4c9fe0e28889d6923f900ceadbb5286f0c6fcd
ikageng tladi <ikagengt.dev@gmail.com>
2026-05-13 19:37:05 +0200

    test: add currency service tests including valid amount, negative amount, api failure, and zero amount

--- GLMS.Tests/CurrencyServiceTests.cs
+++ GLMS.Tests/CurrencyServiceTests.cs
@@ -1,11 +1,7 @@
-﻿using GLMS.Web.Services;
+﻿using GLMS.Tests.Helpers;
+using GLMS.Web.Services;
 using Microsoft.Extensions.Logging;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GLMS.Tests
 {
@@ -16,6 +12,54 @@ namespace GLMS.Tests
         public CurrencyServiceTests()
         {
             _loggerMock = new Mock<ILogger<ExchangeRateService>>();
+        }
+
+        [Fact]
+        public async Task ConvertUsdToZarAsync_ValidAmount_ReturnsConvertedValue()
+        {
+            var handler = new MockHttpMessageHandler(18.50m);
+            var httpClient = new HttpClient(handler);
+            var service = new ExchangeRateService(httpClient, _loggerMock.Object);
+
+            var result = await service.ConvertUsdToZarAsync(100m);
+
+            Assert.Equal(1850.00m, result);
+        }
+
+        [Fact]
+        public async Task ConvertUsdToZarAsync_ZeroAmount_ReturnsZero()
+        {
+            var handler = new MockHttpMessageHandler(18.50m);
+            var httpClient = new HttpClient(handler);
+            var service = new ExchangeRateService(httpClient, _loggerMock.Object);
+
+            var result = await service.ConvertUsdToZarAsync(0m);
+
+            Assert.Equal(0m, result);
+        }
+
+        [Fact]
+        public async Task ConvertUsdToZarAsync_NegativeAmount_ReturnsZero()
+        {
+            var handler = new MockHttpMessageHandler(18.50m);
+            var httpClient = new HttpClient(handler);
+            var service = new ExchangeRateService(httpClient, _loggerMock.Object);
+
+            var result = await service.ConvertUsdToZarAsync(-50m);
+
+            Assert.Equal(0m, result);
+        }
+
+        [Fact]
+        public async Task ConvertUsdToZarAsync_ApiFailure_UsesFallbackRate()
+        {
+            var handler = new FailingHttpMessageHandler();
+            var httpClient = new HttpClient(handler);
+            var service = new ExchangeRateService(httpClient, _loggerMock.Object);
+
+            var result = await service.ConvertUsdToZarAsync(100m);
+
+            Assert.Equal(1850.00m, result);
         }
     }
 }
