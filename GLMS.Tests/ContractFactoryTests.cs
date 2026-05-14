096da65c96180ea15359c4d7035526d99b7efa22
ikageng tladi <ikagengt.dev@gmail.com>
2026-05-14 18:16:34 +0200

    tests: add ContractFactoryTests

---
+++ GLMS.Tests/ContractFactoryTests.cs
@@ -0,0 +1,10 @@
+﻿using GLMS.Web.Factories;
+using GLMS.Web.Models;
+
+namespace GLMS.Tests
+{
+    public class ContractFactoryTests
+    {
+        private readonly DateTime _today = DateTime.Today;
+    }
+}
