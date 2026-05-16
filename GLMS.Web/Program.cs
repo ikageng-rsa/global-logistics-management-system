cec1eeb85b47a828bc1a740813fc2158d6a0d61f
ikageng tladi <ikagengt.dev@gmail.com>
2026-05-16 17:58:13 +0200

    feat: use authentication

--- GLMS.Web/Program.cs
+++ GLMS.Web/Program.cs
@@ -89,11 +89,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseSession();
-
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
