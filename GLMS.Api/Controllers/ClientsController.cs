5623caa8def92cf0b99559fb01a928b900c16966
ikageng tladi <ikagengt.dev@gmail.com>
2026-06-13 19:11:45 +0200

    feat: add ClientsController with GET endpoints

---
+++ GLMS.Api/Controllers/ClientsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GLMS.Api.Models;
+using GLMS.Api.Repositories.Contracts;
+
+namespace GLMS.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientsController : ControllerBase
+    {
+        private readonly IClientRepository _clientRepository;
+
+        public ClientsController(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        // GET: api/clients
+        [HttpGet]
+        public ActionResult<IEnumerable<Client>> GetAll()
+        {
+            var clients = _clientRepository.GetAll();
+            return Ok(clients);
+        }
+
+        // GET: api/clients/5
+        [HttpGet("{id}")]
+        public ActionResult<Client> GetById(int id)
+        {
+            var client = _clientRepository.GetById(id);
+
+            if (client == null)
+                return NotFound();
+
+            return Ok(client);
+        }
+    }
+}
