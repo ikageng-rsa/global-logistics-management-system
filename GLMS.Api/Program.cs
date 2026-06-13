01f99e89441ce21c3d1ea33490edad1ddfdb65d4
ikageng tladi <ikagengt.dev@gmail.com>
2026-06-13 19:19:01 +0200

    Fix: Configure JSON serializer to ignore cycles

--- GLMS.Api/Program.cs
+++ GLMS.Api/Program.cs
@@ -56,7 +56,12 @@ builder.Services.AddHttpClient<ICurrencyService, ExchangeRateService>();
 // Register file service
 builder.Services.AddScoped<IFileService, FileService>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
