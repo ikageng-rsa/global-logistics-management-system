385a905a54ee773f3b551c982368229372b30508
ikageng tladi <ikagengt.dev@gmail.com>
2026-06-13 18:08:08 +0200

    feat: add AppDbContextFactory for EF migrations

---
+++ GLMS.Api/Data/AppDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using GLMS.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace GLMS.Api.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
