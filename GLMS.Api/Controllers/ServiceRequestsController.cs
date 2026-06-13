176c4c556c06c381eeea3a71abc300c157acbb1e
ikageng tladi <ikagengt.dev@gmail.com>
2026-06-13 19:55:23 +0200

    feat: add ServiceRequestsController with GET endpoints

---
+++ GLMS.Api/Controllers/ServiceRequestsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GLMS.Api.Models;
+using GLMS.Api.Repositories.Contracts;
+
+namespace GLMS.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ServiceRequestsController : ControllerBase
+    {
+        private readonly IServiceRequestRepository _serviceRequestRepository;
+        private readonly IContractRepository _contractRepository;
+
+        public ServiceRequestsController(
+            IServiceRequestRepository serviceRequestRepository,
+            IContractRepository contractRepository)
+        {
+            _serviceRequestRepository = serviceRequestRepository;
+            _contractRepository = contractRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ServiceRequest>> GetAll()
+        {
+            var requests = _serviceRequestRepository.GetAll();
+            return Ok(requests);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ServiceRequest> GetById(int id)
+        {
+            var request = _serviceRequestRepository.GetById(id);
+
+            if (request == null)
+                return NotFound();
+
+            return Ok(request);
+        }
+    }
+}
