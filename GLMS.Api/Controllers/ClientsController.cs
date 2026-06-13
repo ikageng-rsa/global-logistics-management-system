c8b42c2db57420428bfd4aebaf5a71953c331f63
ikageng tladi <ikagengt.dev@gmail.com>
2026-06-13 19:18:43 +0200

    feat: add POST, PUT, DELETE endpoints to ClientsController

--- GLMS.Api/Controllers/ClientsController.cs
+++ GLMS.Api/Controllers/ClientsController.cs
@@ -15,7 +15,6 @@ namespace GLMS.Api.Controllers
             _clientRepository = clientRepository;
         }
 
-        // GET: api/clients
         [HttpGet]
         public ActionResult<IEnumerable<Client>> GetAll()
         {
@@ -23,7 +22,6 @@ namespace GLMS.Api.Controllers
             return Ok(clients);
         }
 
-        // GET: api/clients/5
         [HttpGet("{id}")]
         public ActionResult<Client> GetById(int id)
         {
@@ -33,6 +31,47 @@ namespace GLMS.Api.Controllers
                 return NotFound();
 
             return Ok(client);
+        }
+
+        [HttpPost]
+        public ActionResult<Client> Create([FromBody] Client client)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _clientRepository.Add(client);
+            _clientRepository.Save();
+
+            return CreatedAtAction(nameof(GetById), new { id = client.Id }, client);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Client client)
+        {
+            if (id != client.Id)
+                return BadRequest("ID in route does not match ID in body.");
+
+            var existing = _clientRepository.GetById(id);
+            if (existing == null)
+                return NotFound();
+
+            _clientRepository.Update(client);
+            _clientRepository.Save();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existing = _clientRepository.GetById(id);
+            if (existing == null)
+                return NotFound();
+
+            _clientRepository.Delete(id);
+            _clientRepository.Save();
+
+            return NoContent();
         }
     }
 }
