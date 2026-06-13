815cf1de6d5d6d2d26e4d69872104f3ab4331491
ikageng tladi <ikagengt.dev@gmail.com>
2026-06-13 19:25:08 +0200

    feat: add CreateContractRequest DTO

---
+++ GLMS.Api/DTOs/CreateContractRequest.cs
@@ -0,0 +1,12 @@
+﻿using GLMS.Api.Enums;
+
+namespace GLMS.Api.DTOs
+{
+    public class CreateContractRequest
+    {
+        public int ClientId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public ServiceLevel ServiceLevel { get; set; }
+    }
+}
