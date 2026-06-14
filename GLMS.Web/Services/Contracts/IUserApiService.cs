3fa2dee0fad8a64b2fe9eaf0a1b4423922f80821
ikageng tladi <ikagengt.dev@gmail.com>
2026-06-15 00:01:12 +0200

    feat: add User models and IUserApiService interface

---
+++ GLMS.Web/Services/Contracts/IUserApiService.cs
@@ -0,0 +1,14 @@
+﻿using GLMS.Web.Models;
+using GLMS.Web.ViewModels;
+
+namespace GLMS.Web.Services.Contracts
+{
+    public interface IUserApiService
+    {
+        Task<IEnumerable<UserListItem>> GetAllAsync();
+        Task<IEnumerable<string>> GetRolesAsync();
+        Task<(bool Success, string? Error)> CreateAsync(UserCreateViewModel model);
+        Task<(bool Success, string? Error)> UpdateAsync(UserEditViewModel model);
+        Task<(bool Success, string? Error)> DeleteAsync(string id);
+    }
+}
