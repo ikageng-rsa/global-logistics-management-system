7e9798dd84ca6f96843798724cb1f8f160ea50fc
ikageng tladi <ikagengt.dev@gmail.com>
2026-06-14 23:33:56 +0200

    feat: add IServiceRequestApiService interface

---
+++ GLMS.Web/Services/Contracts/IServiceRequestApiService.cs
@@ -0,0 +1,13 @@
+﻿using GLMS.Web.Models;
+
+namespace GLMS.Web.Services.Contracts
+{
+    public interface IServiceRequestApiService
+    {
+        Task<IEnumerable<ServiceRequest>> GetAllAsync();
+        Task<ServiceRequest?> GetByIdAsync(int id);
+        Task<(bool Success, string? Error)> CreateAsync(ServiceRequest serviceRequest);
+        Task<(bool Success, string? Error)> UpdateAsync(ServiceRequest serviceRequest);
+        Task<bool> DeleteAsync(int id);
+    }
+}
