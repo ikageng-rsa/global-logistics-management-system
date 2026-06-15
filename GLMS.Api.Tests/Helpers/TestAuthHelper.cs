e01237873ba0957d60cc977cccc7ea8549d247c4
ikageng tladi <ikagengt.dev@gmail.com>
2026-06-16 01:10:23 +0200

    test: add TestAuthHelper to retrieve JWT for authenticated tests

---
+++ GLMS.Api.Tests/Helpers/TestAuthHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Json;
+
+namespace GLMS.Api.Tests.Helpers
+{
+    public static class TestAuthHelper
+    {
+        public static async Task<string> GetAdminTokenAsync(HttpClient client)
+        {
+            var response = await client.PostAsJsonAsync("api/auth/login", new
+            {
+                email = "admin@techmore.co.za",
+                password = "Admin@123"
+            });
+
+            var result = await response.Content
+                .ReadFromJsonAsync<Dictionary<string, string>>();
+
+            return result?["token"] ?? string.Empty;
+        }
+    }
+}
