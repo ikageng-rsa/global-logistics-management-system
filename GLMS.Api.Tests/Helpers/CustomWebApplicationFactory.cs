add75cd5e7d589839c1f26790543a94196e1e2ea
ikageng tladi <ikagengt.dev@gmail.com>
2026-06-16 01:07:52 +0200

    test: add WebApplicationFactory setup

---
+++ GLMS.Api.Tests/Helpers/CustomWebApplicationFactory.cs
@@ -0,0 +1,37 @@
+﻿using GLMS.Api.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace GLMS.Api.Tests.Helpers
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Remove the real SQL Server DbContext registration
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                // Replace with in-memory database for testing
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDb");
+                });
+
+                // Seed test data
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.EnsureCreated();
+            });
+        }
+    }
+}
--- GLMS.Api/Program.cs
+++ GLMS.Api/Program.cs
@@ -120,3 +120,5 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.Run();
+
+public partial class Program { }
